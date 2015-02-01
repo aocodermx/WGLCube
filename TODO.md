c96fbbddb1d37e4005093bfe1325bcca2a03c698
H182@ <humberto.aparicio.o@gmail.com>
2015-02-01 00:37:49 -0600

    Steps parser done, UX addapted for responsive design. Before animation implementation.

--- TODO.md
+++ TODO.md
@@ -1,13 +1,8 @@
 # TODO for rubikgl-web
 
-Create a 3D model for original rubiks cube, and variations.
-
-## Logic
-Create a parser for strigs of movements for any size of rubiks cube. Some links:  
-  - http://ruwix.com/the-rubiks-cube/notation/
-  - http://ruwix.com/the-rubiks-cube/notation/advanced/
-
 Create a connection with WCA scrambler.
+
+Create a 3D model for original rubiks cube, and variations.
 
 Create a namespace for the cube and a friendly plugin API.
 
@@ -19,3 +14,10 @@ Add a indicator for faces.
 Add animations.
 
 Make it portable, embeddable and easy to use in custom pages.
+
+# Done
+
+## Logic
+Create a parser for strigs of movements for any size of rubiks cube. Some links:  
+  - http://ruwix.com/the-rubiks-cube/notation/
+  - http://ruwix.com/the-rubiks-cube/notation/advanced/
--- rubikgl-web.js
+++ rubikgl-web.js
@@ -1,137 +1,244 @@
-  var cubeSize  = 3;
-  var cubeArray = Array();
+/*
+var WGLCUBE = WGLCUBE || (function(){
+  return {
+    REVISION: 1
+  };
+})();
+
+WGLCUBE.Cube = function(){
+  var size = 3;
+  var array = new Array();
+};
+*/
+var cubeSize  = 3;
+var cubeArray = new Array();
+var scene     = new THREE.Scene();
+
+function setupWebGL(){
+  var renderer  = window.WebGLRenderingContext ? new THREE.WebGLRenderer({antialias:true}) : new THREE.CanvasRenderercamera();
   var container = document.getElementById("rubik-cube");
-  var scene     = new THREE.Scene();
-  var renderer  = window.WebGLRenderingContext ? new THREE.WebGLRenderer() : new THREE.CanvasRenderer();
   var camera    = new THREE.PerspectiveCamera( 45, container.offsetWidth / (container.offsetHeight), 0.1, 1000 );
   var controls  = new THREE.OrbitControls(camera, renderer.domElement);
 
-  function setupWebGL(){
-    camera.position.addScalar(cubeSize * 1.4);
-    renderer.setClearColor(0xDDDDDD, 1);
-    renderer.setSize( container.offsetWidth, container.offsetHeight );
-    container.appendChild(renderer.domElement);
+  window.addEventListener('resize', function() {
+    var WIDTH  = container.offsetWidth;
+    var HEIGHT = container.offsetHeight
+    renderer.setSize(WIDTH, HEIGHT);
+    camera.aspect = WIDTH / HEIGHT;
+    camera.updateProjectionMatrix();
+    adjustControls();
+  });
 
-    window.addEventListener('resize', function() {
-      var WIDTH  = container.offsetWidth;
-      var HEIGHT = container.offsetHeight
-      renderer.setSize(WIDTH, HEIGHT);
-      camera.aspect = WIDTH / HEIGHT;
-      camera.updateProjectionMatrix();
-    });
+  camera.position.addScalar(cubeSize * 1.4);
+  renderer.setClearColor(0xDDDDDD, 1);
+  renderer.setSize( container.offsetWidth, container.offsetHeight );
+  container.appendChild(renderer.domElement);
+  controls.noPan = true;
 
-    renderCube(scene);
-    runGL();
+  makeCube(scene);
+  adjustControls();
+
+  if(window.self != window.top && window.frameElement.tagName == "IFRAME"){
+    var initString = window.frameElement.getAttribute("data-start");
+    var stepString = window.frameElement.getAttribute("data-steps");
+    var rubikSteps = document.getElementById('rubik-steps');
+    rubikSteps.innerHTML = stepString;
+    applySteps(initString);
+  } else {
+    // On Top
   }
 
-  function renderCube(scene){
-    var geometry = new THREE.BoxGeometry( 0.98, 0.98, 0.98 );
-    var material = new THREE.MeshFaceMaterial( [
-      new THREE.MeshBasicMaterial({color:0xFF6600}), // Right  -> Orange
-      new THREE.MeshBasicMaterial({color:0xFF0000}), // Left   -> Red
-      new THREE.MeshBasicMaterial({color:0x00FF00}), // Top    -> Green
-      new THREE.MeshBasicMaterial({color:0x0000FF}), // Bottom -> Blue
-      new THREE.MeshBasicMaterial({color:0xFFFFFF}), // Front  -> White
-      new THREE.MeshBasicMaterial({color:0xFFFF00})  // Back   -> Yellow
-      ] );
+  runGL(renderer, camera, controls);
+}
 
-    for(var z=-cubeSize/2 + 0.5; z<cubeSize/2; z++){
-      for(var x=-cubeSize/2 + 0.5; x<cubeSize/2; x++){
-        for(var y=-cubeSize/2 + 0.5; y<cubeSize/2; y++){
-          var piece = new THREE.Mesh(geometry, material);
-          piece.position.set(x, y, z);
-          cubeArray.push(piece);
-          scene.add(piece);
-        }
+function adjustControls(){
+  var rubikControl = document.getElementById('rubik-control');
+  var controlButtons = rubikControl.getElementsByTagName('button');
+  var height = (controlButtons[0].offsetHeight * 0.6) + "px";
+  for (var i=0; i<controlButtons.length;i++){
+    controlButtons[i].style.fontSize = height;
+  }
+}
+
+function makeCube(scene){
+  var geometry = new THREE.BoxGeometry( 0.98, 0.98, 0.98 );
+  var material = new THREE.MeshFaceMaterial( [
+    new THREE.MeshBasicMaterial({color:0xFF6600}), // Right  -> Orange
+    new THREE.MeshBasicMaterial({color:0xFF0000}), // Left   -> Red
+    new THREE.MeshBasicMaterial({color:0x00FF00}), // Top    -> Green
+    new THREE.MeshBasicMaterial({color:0x0000FF}), // Bottom -> Blue
+    new THREE.MeshBasicMaterial({color:0xFFFFFF}), // Front  -> White
+    new THREE.MeshBasicMaterial({color:0xFFFF00})  // Back   -> Yellow
+    ] );
+
+  for(var z=-cubeSize/2 + 0.5; z<cubeSize/2; z++){
+    for(var x=-cubeSize/2 + 0.5; x<cubeSize/2; x++){
+      for(var y=-cubeSize/2 + 0.5; y<cubeSize/2; y++){
+        var piece = new THREE.Mesh(geometry, material);
+        piece.position.set(x, y, z);
+        cubeArray.push(piece);
+        scene.add(piece);
       }
     }
   }
+}
 
-  function moveOnAxis(axis, level, direction){
-    var level = level - cubeSize/2 + 0.5;
-    var core = new THREE.Object3D();
+function moveOnAxis(axis, level, direction){
+  var level = level - cubeSize/2 + 0.5 - 1;
+  var core = new THREE.Object3D();
 
-    scene.add(core);
+  if(axis != "x" && axis != "y" && axis != "z")
+    return -1;
 
-    if(axis != "x" && axis != "y" && axis != "z")
-      return -1;
+  scene.add(core);
 
-    for (var b=0; b<cubeArray.length; b++)
-      if( Math.round(cubeArray[b].getWorldPosition()[axis]) === level )
-        THREE.SceneUtils.attach(cubeArray[b], scene, core);
+  for (var b=0; b<cubeArray.length; b++)
+    if( Math.round(cubeArray[b].getWorldPosition()[axis]) === level )
+      THREE.SceneUtils.attach(cubeArray[b], scene, core);
 
-    // For performance (90*(Math.PI/180) = 1.5707963267948966
-    switch(axis){
-      case "x":
-        core.rotation.x += direction*1.5707963267948966;
-        break;
-      case "y":
-        core.rotation.y += direction*1.5707963267948966;
-        break;
-      case "z":
-        core.rotation.z += direction*1.5707963267948966;
-        break;
-    }
-
-    core.updateMatrixWorld();
-
-    for (var b=0; b<cubeArray.length; b++)
-      if( Math.round(cubeArray[b].getWorldPosition()[axis]) === level )
-        THREE.SceneUtils.detach(cubeArray[b], core, scene);
-
-    //console.log("Scene objects:", scene.children.length);
-    //console.log("Core  objects:", core.children.length);
-    scene.remove(core);
+  // For performance (90*(Math.PI/180) = 1.5707963267948966
+  switch(axis){
+    case "x":
+      core.rotation.x += direction*1.5707963267948966;
+      break;
+    case "y":
+      core.rotation.y += direction*1.5707963267948966;
+      break;
+    case "z":
+      core.rotation.z += direction*1.5707963267948966;
+      break;
   }
 
-  function moveString(steps){
-    for (var step in steps){
-      switch(steps[step]){
-        case "L":
-          moveOnAxis("x", 0, 1);
+  core.updateMatrixWorld();
+
+  for (var b=0; b<cubeArray.length; b++)
+    if( Math.round(cubeArray[b].getWorldPosition()[axis]) === level )
+      THREE.SceneUtils.detach(cubeArray[b], core, scene);
+
+  scene.remove(core);
+}
+
+function applySteps(steps){
+  if(steps === null)
+    return -1;
+
+  if(steps.length == 0)
+    return -1;
+
+  if(steps === "*") // Randomize
+    steps = "FUR";  // Steps from internet or algorithm
+
+  steps += ' ';
+  var pos = 0;
+  var char = '';
+  var tok = '';
+
+  var STATE_START = 0;
+  var STATE_LAYER = 1;
+  var STATE_AXIS  = 2;
+  var STATE_TIMES = 3;
+  var STATE_DIRECTION = 4;
+  var STATE_END = 5;
+
+  while(typeof char !== "undefined"){
+    var step_axis = '';
+    var step_layer = 1;
+    var step_direction = 1;
+    var step_times = 1;
+    var STATE = STATE_START;
+
+    while(STATE !== STATE_END){
+      char = steps[pos];
+      //console.log("CHAR:",char, " STATE:", STATE);
+      switch(STATE){
+        case STATE_START:
+          if(!/\s/g.test(char)){
+            STATE = STATE_LAYER;
+            pos--;
+          }
+          if(typeof char === "undefined"){
+            return;
+          }
           break;
-        case "l":
-          moveOnAxis("x", 0,-1);
+        case STATE_LAYER:
+          var result = parseInt(char);
+          if(isNaN(result)){
+            STATE = STATE_AXIS;
+            pos--;
+            if(tok != ''){
+              step_layer = parseInt(tok);
+              tok = '';
+            }
+          }
+          else{
+            tok += char;
+          }
           break;
-        case "R":
-          moveOnAxis("x", 2,-1);
+        case STATE_AXIS:
+          switch(char){
+            case 'R':
+              step_layer = cubeSize - step_layer + 1;
+              step_direction *= -1;
+            case 'L':
+              step_axis = 'x';
+              break;
+            case 'U':
+              step_layer = cubeSize - step_layer + 1;
+              step_direction *= -1;
+            case 'D':
+              step_axis = 'y';
+              break;
+            case 'F':
+              step_layer = cubeSize - step_layer + 1;
+              step_direction *= -1;
+            case 'B':
+              step_axis = 'z';
+              break;
+            default:
+              console.log("FACE to move is incorrect");
+              return -1;
+          }
+          STATE = STATE_TIMES;
           break;
-        case "r":
-          moveOnAxis("x", 2, 1);
+        case STATE_TIMES:
+          var result = parseInt(char);
+          if(isNaN(result)){
+            STATE = STATE_DIRECTION;
+            pos--;
+            if(tok != ''){
+              step_times = parseInt(tok);
+              tok = '';
+            }
+          }
+          else{
+            tok += char;
+          }
           break;
-        case "U":
-          moveOnAxis("y", 2,-1);
-          break;
-        case "u":
-          moveOnAxis("y", 2, 1);
-          break;
-        case "D":
-          moveOnAxis("y", 0, 1);
-          break;
-        case "d":
-          moveOnAxis("y", 0,-1);
-          break;
-        case "F":
-          moveOnAxis("z", 2,-1);
-          break;
-        case "f":
-          moveOnAxis("z", 2, 1);
-          break;
-        case "B":
-          moveOnAxis("z", 0, 1);
-          break;
-        case "b":
-          moveOnAxis("z", 0,-1);
+        case STATE_DIRECTION:
+          if(char === '\'')
+            step_direction *= -1;
+          else
+            pos --;
+          STATE = STATE_END;
           break;
         default:
-          console.log("Invalid character", steps[step]);
+          console.log("Fatal error, state unknown.");
           return -1;
       }
+      pos++;
     }
+    for (var d=0; d<step_times; d++)
+      moveOnAxis(step_axis, step_layer, step_direction);
   }
+}
 
-  function runGL() {
-    requestAnimationFrame( runGL );
-    renderer.render( scene, camera );
-    controls.update();
-  }
+function runGL(renderer, camera, controls) {
+  requestAnimationFrame( function(){runGL(renderer, camera, controls);} );
+  renderer.render( scene, camera );
+  //TWEEN.update();
+  controls.update();
+}
+
+if(typeof module !== 'undefined' && module.exports) {
+  module.exports = WGLCUBE;
+}
