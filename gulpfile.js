674341f151bf5b0b2795c5949df3ac1903680485
aocodermx <aocodermx@gmail.com>
2016-11-21 23:38:56 -0600

    Manager implemented to avoid more than 16 WebGL context use and to display various cubes at the same time. README and LICENSE files added.

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,35 @@
-var fs     = require ( 'fs' );
-var gulp   = require ( 'gulp' );
-var jshint = require ( 'gulp-jshint' );
-var concat = require ( 'gulp-concat' );
-var rename = require ( 'gulp-rename' );
-var uglify = require ( 'gulp-uglify' );
+var fs        = require ( 'fs' );
+var gulp      = require ( 'gulp' );
+var jshint    = require ( 'gulp-jshint' );
+var concat    = require ( 'gulp-concat' );
+var rename    = require ( 'gulp-rename' );
+var uglify    = require ( 'gulp-uglify' );
+var uglifycss = require('gulp-uglifycss');
 
 var browserSync = require ( 'browser-sync' ).create ( );
 
 
 var json = JSON.parse ( fs.readFileSync ( './package.json' ) );
 
+gulp.task ( 'css', function ( ) {
+    return gulp.src ( 'css/src/*.css' )
+        .pipe ( uglifycss ( ) )
+        .pipe ( concat ( 'wglcube.css' ) )
+        .pipe ( rename ( 'wglcube_V' + json.version + '.min.css' ) )
+        .pipe ( gulp.dest ( 'css' ) );
+} );
 
 gulp.task ( 'js', function ( ) {
     return gulp.src ( 'js/src/*.js' )
         .pipe ( jshint ( ) )
         .pipe ( jshint.reporter ( 'default' ) )
-        // .pipe ( uglify ( ) ) // Comment to debug.
+        .pipe ( uglify ( ) ) // Comment to debug.
         .pipe ( concat ('wglcube.js' ) )
-        .pipe ( rename ( 'wglcube_'+ json.version +'.min.js' ) )
+        .pipe ( rename ( 'wglcube_V'+ json.version +'.min.js' ) )
         .pipe ( gulp.dest ( 'js' ) );
 } );
 
-gulp.task ( 'js-watch', [ 'js' ], function ( done ) {
+gulp.task ( 'js-watch', [ 'js', 'css' ], function ( done ) {
     browserSync.reload ( );
     done ( );
 } );
--- js/app.js
+++ js/app.js
@@ -1,4 +1,2 @@
 
 // var cube1 = new WGL.Cube ( document.getElementsByClassName( 'wglcube' )[0] );
-
-var player1 = new WGL.Player ( document.getElementsByClassName ( 'wglcube' )[0] );
